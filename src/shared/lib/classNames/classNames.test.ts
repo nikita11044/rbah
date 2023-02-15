b2f6e13da33b0d76b87945100f991b790142c38b
nikita <nikita11042000@gmail.com>
2023-02-15 16:02:17 +0300

    feat: added jest

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,27 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with cls', () => {
+        expect(classNames('testClass')).toBe('testClass');
+    });
+
+    test('with additional', () => {
+        const expected = 'testClass additionalClass1 additionalClass2';
+        expect(classNames('testClass', {}, ['additionalClass1', 'additionalClass2'])).toBe(expected);
+    });
+
+    test('with mods', () => {
+        const expected = 'testClass additionalClass1 additionalClass2 scroll hover';
+        expect(classNames('testClass', { scroll: true, hover: true }, ['additionalClass1', 'additionalClass2'])).toBe(expected);
+    });
+
+    test('with false mod', () => {
+        const expected = 'testClass additionalClass1 additionalClass2 hover';
+        expect(classNames('testClass', { scroll: false, hover: true }, ['additionalClass1', 'additionalClass2'])).toBe(expected);
+    });
+
+    test('with undefined mod', () => {
+        const expected = 'testClass additionalClass1 additionalClass2 scroll';
+        expect(classNames('testClass', { scroll: true, hover: undefined }, ['additionalClass1', 'additionalClass2'])).toBe(expected);
+    });
+});
