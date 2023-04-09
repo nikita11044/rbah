317d65f1a4224ee9dca4c4ce4a9cc312fd910e4d
nikita <nikita11042000@gmail.com>
2023-04-09 18:41:53 +0300

    feat: added purple theme

--- src/app/providers/ThemeProvider/lib/ThemeCtx.ts
+++ src/app/providers/ThemeProvider/lib/ThemeCtx.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 export enum Theme {
     LIGHT = 'light_theme',
     DARK = 'dark_theme',
+    PURPLE = 'purple_theme'
 }
 
 export interface ThemeCtxProps {
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,7 +10,18 @@ export function useTheme(): UseThemeReturnType {
     const { theme, setTheme } = useContext(ThemeCtx);
 
     const changeTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let newTheme: Theme;
+        switch (theme) {
+        case Theme.DARK:
+            newTheme = Theme.LIGHT;
+            break;
+        case Theme.LIGHT:
+            newTheme = Theme.PURPLE;
+            break;
+        default:
+            newTheme = Theme.DARK;
+            break;
+        }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
